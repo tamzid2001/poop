346dd6062fb7e1688931dc67207fb6be8bfddade
Arif <arif@192.168.1.8>
2015-07-21 18:10:41 -0400

    poop

--- xxxxx/xflaskx.py
+++ xxxxx/xflaskx.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import weatherstuff 
 
 weather = Flask(__name__)
 
-@weather.route('/weather')
+
+
+@weather.route('/', methods = ['POST','GET'])
 def get_weather():
-    a = weatherstuff.city("Paris")
-    return render_template('webweather.html', temp = a)
+    if request.method == 'GET':
+        return render_template('webweather.html')
+    else:
+        formx = request.form
+        city = formx['cityx']
+    a = weatherstuff.city(city)
+    t = a['main']['temp']
+    return render_template('webweather.html', temp = t)
 
 
 
 if __name__ == '__main__':
     weather.debug = True
-    weather.run()
+    weather.run(host = '0.0.0.0')
